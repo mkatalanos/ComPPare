827f1ec9419b2590e180289d76038b3307d5040a
funglf <stanleyfunglf@gmail.com>
2025-07-26 20:33:22 +0100

    moved instance() into private member
    Signed-off-by: funglf <stanleyfunglf@gmail.com>

--- include/comppare/internal/config.hpp
+++ include/comppare/internal/config.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <chrono>
+#include <utility>
 #include <string_view>
 
 namespace comppare
@@ -10,16 +11,10 @@ namespace comppare
         using clock_t = std::chrono::steady_clock;
         using time_point_t = std::chrono::time_point<clock_t>;
 
-        config(const config&) = delete;
-        config(config&&) = delete;
-        config& operator=(const config&) = delete;
-        config& operator=(config&&) = delete;
-
-        static config &instance()
-        {
-            static config inst;
-            return inst;
-        }
+        config(const config &) = delete;
+        config(config &&) = delete;
+        config &operator=(const config &) = delete;
+        config &operator=(config &&) = delete;
 
         static uint64_t warmup_iters() { return instance().warmup_iters_; }
         static void set_warmup_iters(uint64_t v) { instance().warmup_iters_ = v; }
@@ -27,17 +22,29 @@ namespace comppare
         static uint64_t bench_iters() { return instance().bench_iters_; }
         static void set_bench_iters(uint64_t v) { instance().bench_iters_ = v; }
 
-        static void set_roi_us(const time_point_t& start, const time_point_t& end) { instance().roi_ = std::chrono::duration<double, std::micro>(end - start).count(); }
-        static double get_roi_us() { 
-            double us = instance().roi_;
-            instance().roi_ = double(0.0);
-            return us;
-        }
+        static void reset_roi_us() { instance().roi_ = double(0.0); }
+        
+        static void set_roi_us(const time_point_t &start, const time_point_t &end) { instance().roi_ = std::chrono::duration<double, std::micro>(end - start).count(); }
+        static void set_roi_us(const float start, const float end) { instance().roi_ = static_cast<double>(end - start); }
+        static void set_roi_us(const double start, const double end) { instance().roi_ = end - start; }
+        static void set_roi_us(const double v) { instance().roi_ = v; }
+        static void set_roi_us(const float v) { instance().roi_ = static_cast<double>(v); }
+
+        static void increment_roi_us(const double v) { instance().roi_ += v; }
+        static void increment_roi_us(const float v) { instance().roi_ += static_cast<double>(v); }
+
+        static double get_roi_us() { return instance().roi_; }
 
     private:
         config() = default;
 
-        double roi_{0};
+        static config &instance()
+        {
+            static config inst;
+            return inst;
+        }
+
+        double roi_{0.0};
         uint64_t warmup_iters_{100};
         uint64_t bench_iters_{100};
     };
@@ -45,16 +52,10 @@ namespace comppare
     class current_state
     {
     public:
-        current_state(const current_state&) = delete;
-        current_state(current_state&&) = delete;
-        current_state& operator=(const current_state&) = delete;
-        current_state& operator=(current_state&&) = delete;
-
-        static current_state &instance()
-        {
-            static current_state inst;
-            return inst;
-        }
+        current_state(const current_state &) = delete;
+        current_state(current_state &&) = delete;
+        current_state &operator=(const current_state &) = delete;
+        current_state &operator=(current_state &&) = delete;
 
         static bool using_plugin() { return instance().using_plugin_; }
         static void set_using_plugin(bool v) { instance().using_plugin_ = v; }
@@ -62,8 +63,14 @@ namespace comppare
         static std::string_view impl_name() { return instance().impl_name_; }
         static void set_impl_name(std::string_view name) { instance().impl_name_ = name; }
 
-        private:
+    private:
         current_state() = default;
+
+        static current_state &instance()
+        {
+            static current_state inst;
+            return inst;
+        }
 
         bool using_plugin_{false};
         std::string_view impl_name_;
