4748aa92d8665490db6ad18d5bc47b21110894e2
Marios <mkatalanos@gmail.com>
2025-08-22 00:47:36 +0100

    Made use of argp.h for argument handling

--- include/comppare/internal/helper.hpp
+++ include/comppare/internal/helper.hpp
@@ -2,127 +2,91 @@
 
 Copyright 2025 | Leong Fan FUNG | funglf | stanleyfunglf@gmail.com
 
-Permission is hereby granted, free of charge, to any person obtaining a copy 
-of this software and associated documentation files (the “Software”), to deal 
-in the Software without restriction, including without limitation the rights 
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-copies of the Software, and to permit persons to whom the Software is 
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the “Software”), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in 
+The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
 */
 #pragma once
+#include <argp.h>
 #include <charconv>
+#include <cstdint>
+#include <stdexcept>
 #include <string>
 #include <string_view>
-#include <stdexcept>
-#include <cstdint>
 
 #include <comppare/internal/config.hpp>
 
-namespace comppare::internal::helper
-{
-    template <typename T>
-    T get_arg_value(std::string_view option, const char *nextArg)
-    {
-        std::string_view valstr;
-        if (auto eq = option.find('='); eq != std::string_view::npos)
-            valstr = option.substr(eq + 1);
-        else if (nextArg)
-            valstr = nextArg;
-        else
-            throw std::invalid_argument(std::string(option) + " requires a value");
+namespace comppare::internal::helper {
 
-        
-        if constexpr (std::same_as<T, std::string>)
-        {
-            return std::string(valstr);
-        }
-        else if constexpr (std::is_integral_v<T>) // if integral type
-        {
-            size_t idx = 0;
-            std::string s{valstr};
+static struct argp_option options[] = {
+    {"warmup", 'w', "UINT64", 0, "Warmup iterations"},
+    {"iter", 'i', "UINT64", 0, "Number of iterations"},
+    {"tolerance", 't', "LONGDOUBLE", 0, "Tolerance value"},
+    {0}};
 
-            if constexpr (std::is_signed_v<T>) // if signed
-            {
-                long long tmp = std::stoll(s, &idx);
-                if (idx != s.size()) // if not all characters were processed
-                    throw std::invalid_argument("invalid integer for " + std::string(option));
+struct arguments {
+  uint64_t warmup = 0;
+  uint64_t iter = 0;
+  long double tolerance = 0;
 
-                if (tmp < std::numeric_limits<T>::min() ||
-                    tmp > std::numeric_limits<T>::max()) // if out of range for T
-                    throw std::out_of_range("integer out of range for " + std::string(option));
+  bool warmup_set = false;
+  bool iter_set = false;
+  bool tolerance_set = false;
+};
 
-                return static_cast<T>(tmp);
-            }
-            else // unsigned
-            {
-                if (!s.empty() && s.front() == '-') // if negative -- reject
-                    throw std::invalid_argument("invalid unsigned integer for " + std::string(option));
+static error_t parse_opt(int key, char *arg, struct argp_state *state) {
+  arguments *args = static_cast<arguments *>(state->input);
 
-                unsigned long long tmp = std::stoull(s, &idx); 
-                if (idx != s.size()) // if not all characters were processed
-                    throw std::invalid_argument("invalid unsigned integer for " + std::string(option));
+  switch (key) {
+  case 'w':
+    args->warmup = std::strtoull(arg, nullptr, 10);
+    args->warmup_set = true;
+    break;
+  case 'i':
+    args->iter = std::strtoull(arg, nullptr, 10);
+    args->iter_set = true;
+    break;
+  case 't':
+    args->tolerance = std::strtold(arg, nullptr);
+    args->tolerance_set = true;
+    break;
 
-                if (tmp > std::numeric_limits<T>::max()) // if out of range for T
-                    throw std::out_of_range("unsigned integer out of range for " + std::string(option));
+  case ARGP_KEY_ARG:
+    return ARGP_ERR_UNKNOWN; // No positional arguments allowed
+  case ARGP_KEY_END:
+    break;
+  default:
+    return ARGP_ERR_UNKNOWN;
+  }
+  return 0;
+}
 
-                return static_cast<T>(tmp);
-            }
-        }
-        else if constexpr (std::is_floating_point_v<T>)
-        {
-            size_t idx = 0;
-            long double tmp = std::stold(std::string(valstr), &idx);
-            if (idx != valstr.size())
-                throw std::invalid_argument("invalid floating-point for " + std::string(option));
-            return static_cast<T>(tmp);
-        }
-        else
-        {
-            static_assert(std::is_arithmetic_v<T> || std::same_as<T, std::string>,
-                          "get_arg_value supports only arithmetic types or std::string");
-        }
-    }
+static struct argp argp = {options, parse_opt, "", ""};
 
-    static inline void parse_args(int argc, char **argv)
-    {
-        for (int i = 1; i < argc; ++i)
-        {
-            std::string_view arg = argv[i];
+static inline void parse_args(int argc, char **argv) {
+  arguments args;
 
-            auto get_next_arg_if_needed = [&](std::string_view a) -> const char *
-            {
-                return (a.find('=') == std::string_view::npos && i + 1 < argc)
-                           ? argv[++i]
-                           : nullptr;
-            };
-
-            if (arg.rfind("--warmup", 0) == 0)
-            {
-                auto w = get_arg_value<std::uint64_t>(arg, get_next_arg_if_needed(arg));
-                comppare::config::set_warmup_iters(w);
-            }
-            else if (arg.rfind("--iter", 0) == 0)
-            {
-                auto n = get_arg_value<std::uint64_t>(arg, get_next_arg_if_needed(arg));
-                comppare::config::set_bench_iters(n);
-            }
-            else if (arg.rfind("--tolerance", 0) == 0)
-            {
-                auto tol = get_arg_value<long double>(arg, get_next_arg_if_needed(arg));
-                comppare::config::set_all_fp_tolerance(tol);
-            }
-        }
-    }
+  argp_parse(&argp, argc, argv, 0, 0, &args);
+  if (args.warmup_set)
+    comppare::config::set_warmup_iters(args.warmup);
+  if (args.iter_set)
+    comppare::config::set_bench_iters(args.iter);
+  if (args.tolerance_set)
+    comppare::config::set_all_fp_tolerance(args.tolerance);
+}
 } // namespace comppare::internal::helper
